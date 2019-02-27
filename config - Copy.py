9183df53008f168876006604f0499519ce92667a
Ashraful Haque Sulaiman <28388435+Ashraful7679@users.noreply.github.com>
2019-02-28 01:03:45 +0600

    Add files via upload

---
+++ config - Copy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+USERNAME = ''
+PASSWORD = ''
+
+COURSES = [
+        'building-solutions-using-excel-2013-and-access-2013-together',
+        'excel-2013-essential-training',
+        'excel-2013-advanced-formulas-and-functions',
+        'excel-2013-tips-and-tricks',
+        'excel-2013-managing-and-analyzing-data',
+        'excel-2013-macros',
+        'excel-2013-pivot-tables',
+        'creating-interactive-dashboards-in-excel-2013',
+        'excel-2013-shortcuts',
+        'excel-2013-working-with-charts-and-graphs',
+        'excel-2013-data-validation-2',
+        'excel-2013-advanced-formatting-techniques',
+        'excel-2013-managing-multiple-worksheets-and-workbooks',
+        'excel-2013-working-with-dates-and-times',
+        'excel-2013-charts-in-depth',
+        'cleaning-up-your-excel-2013-data'
+        ]
